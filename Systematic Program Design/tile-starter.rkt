caf3a2f125f904fbea6b045680119c9cde69cd3e
RCD2020 <dobertravis@icloud.com>
2024-09-02 16:29:01 -0600

    completed BSL P3

---
+++ Systematic Program Design/tile-starter.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname before) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; Use the DrRacket square, beside and above functions to create an image
+;; like the one in this link:
+;;
+;; https://cs110.students.cs.ubc.ca/bank/tile.png
+;;
+;; If you prefer to be more creative feel free to do so. You can use other
+;; DrRacket image functions to make a more interesting or more attractive image.
+
+(define yellow (square 10 "solid" "yellow"))
+(define blue (square 10 "solid" "blue"))
+
+(above (beside blue yellow)
+       (beside yellow blue))
